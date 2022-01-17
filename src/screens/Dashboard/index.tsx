91b3ecc87d32c3f5ee9c5fa7a6f6535725892818
Luciano Pedesol <lucianopedesol@hotmail.com>
2022-01-17 00:03:16 -0300

    first commit

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+
+} from "./styles";
+
+export function Dashboard() {
+    return (
+        <Container >
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/60025358?v=4' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Luciano</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighlightCard />
+        </Container>
+    )
+}
+
